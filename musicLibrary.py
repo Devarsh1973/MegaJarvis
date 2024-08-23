0eb6dbf4823c3a74c5d3cbd5449b5335361e779b
Devarsh1973 <devarshprajapati5@gmail.com>
2024-08-23 06:27:52 -0400

    Update musicLibrary.py

--- musicLibrary.py
+++ musicLibrary.py
@@ -1,5 +1,6 @@
 music={
     "perfect":"https://www.youtube.com/watch?v=cNGjD0VG4R8",
     "night": "https://www.youtube.com/watch?v=7KPyunRIjr0",
+    "seven":"https://www.youtube.com/watch?v=au1PyhcecWg",
     "seven":"https://www.youtube.com/watch?v=au1PyhcecWg"
 }
